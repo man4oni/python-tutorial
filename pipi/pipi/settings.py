e4aaa34b01b234798095a21d40a1a600fa4b6f51
Mariyan Garvanski <mgarvanski@gmail.com>
2019-11-21 14:14:14 +0200

    django part-1

--- pipi/pipi/settings.py
+++ pipi/pipi/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pepi.apps.PepiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
